394fdd827ca1ad63793c07f54938b16833fd0efe
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-20 08:06:33 +0200

    Read Class Properties into HTML file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-fundamentals-ucdhk';
+  title = 'This is my First App';
+  firstName = 'John';
+  lastName = 'Doe';
 }
